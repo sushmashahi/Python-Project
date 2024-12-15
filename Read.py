5d7c2224a11214febfe2c4a55ac356fd139083ae
sushma kumari <sushmashahi03@gmail.com>
2024-12-16 00:41:25 +0100

    Your commit message

---
+++ Read.py
@@ -0,0 +1,16 @@
+import os
+
+# Path to the file
+f = open("Sentence.csv", "r")
+
+#Here r is used to read the file
+print(f.read(12))
+#print(f.read(5)) It will read the first word of the file 
+print(f.readline())
+print(f.readline())
+for x in f:
+  print(y)
+  f.close()
+
+
+
